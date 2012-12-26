cd27953eb704d40b7b015b14b32fa3abb235fc54
Prof1983 <prof1983@yandex.ru>
2012-12-26 14:58:15 +0600

    Updated AUtilsMain

--- Utils/AUtilsMain.pas
+++ Utils/AUtilsMain.pas
@@ -2,7 +2,7 @@
 @Abstract AUtils - Main
 @Author Prof1983 <prof1983@ya.ru>
 @Created 20.11.2012
-@LastMod 21.11.2012
+@LastMod 26.12.2012
 }
 unit AUtilsMain;
 
@@ -15,11 +15,15 @@ uses
 
 // --- AUtils ---
 
+function AUtils_ExtractFileExtP(const FileName: APascalString): APascalString;
+
 function AUtils_ExtractFileExtWS(const FileName: AWideString): AWideString; stdcall;
 
 function AUtils_ExtractFilePathP(const FileName: APascalString): APascalString; stdcall;
 
 function AUtils_FileExists(const FileName: AString_Type): ABoolean; stdcall;
+
+function AUtils_FileExistsP(const FileName: APascalString): ABoolean;
 
 function AUtils_FileExistsWS(const FileName: AWideString): ABoolean; stdcall;
 
@@ -33,22 +37,74 @@ function AUtils_IntToStrP(Value: AInt): APascalString; stdcall;
 
 function AUtils_IntToStrWS(Value: AInt): AWideString; stdcall;
 
+function AUtils_ReplaceCommaP(const S: APascalString; DecimalSeparator: AChar = #0;
+    ClearSpace: ABoolean = True): APascalString;
+
 function AUtils_ReplaceCommaWS(const S: AWideString; DecimalSeparator: AChar = #0;
     ClearSpace: ABoolean = True): AWideString; stdcall;
 
 function AUtils_Sleep(Milliseconds: AUInt): AError; stdcall;
 
+function AUtils_StrToFloatDefP(const S: APascalString; DefValue: AFloat): AFloat;
+
 function AUtils_StrToFloatDefWS(const S: AWideString; DefValue: AFloat): AFloat; stdcall;
+
+function AUtils_StrToFloatP(const Value: APascalString): AFloat;
+
+function AUtils_StrToIntDefP(const S: APascalString; DefValue: AInt): AInt;
 
 function AUtils_StrToIntDefWS(const S: AWideString; DefValue: AInt): AInt; stdcall;
 
+function AUtils_StrToIntP(const Value: APascalString): AInt;
+
 function AUtils_Trim(var S: AString_Type): AError; stdcall;
+
+function AUtils_TrimP(const S: APascalString): APascalString;
 
 function AUtils_TrimWS(const S: AWideString): AWideString; stdcall;
 
 implementation
 
 // --- AUtils ---
+
+function AUtils_ExtractFileExtP(const FileName: APascalString): APascalString;
+var
+  FN: AString_Type;
+  Res: AString_Type;
+begin
+  if Assigned(AUtilsProcVars.AUtils_ExtractFileExt) then
+  begin
+    if (AString_AssignP(FN, FileName) < 0) then
+    begin
+      Result := '';
+      Exit;
+    end;
+    if (AString_Clear(Res) < 0) then
+    begin
+      Result := '';
+      Exit;
+    end;
+    if (AUtils_ExtractFileExt(FN, Res) < 0) then
+    begin
+      Result := '';
+      Exit;
+    end;
+    Result := AString_ToPascalString(Res);
+    Exit;
+  end;
+  {$ifdef ADepr}
+  if Assigned(AUtilsProcVars.AUtils_ExtractFileExtWS) then
+  begin
+    try
+      Result := AUtilsProcVars.AUtils_ExtractFileExtWS(FileName);
+    except
+      Result := '';
+    end;
+    Exit;
+  end;
+  {$endif}
+  Result := '';
+end;
 
 function AUtils_ExtractFileExtWS(const FileName: AWideString): AWideString;
 var
@@ -80,7 +136,7 @@ begin
     Result := AString_ToWideString(Res);
     Exit;
   end;
-  {$IFDEF ADepr}
+  {$ifdef ADepr}
   if Assigned(AUtilsProcVars.AUtils_ExtractFileExtWS) then
   begin
     try
@@ -90,8 +146,8 @@ begin
     end;
     Exit;
   end;
+  {$endif}
   Result := '';
-  {$ENDIF}
 end;
 
 function AUtils_ExtractFilePathP(const FileName: APascalString): APascalString;
@@ -135,24 +191,67 @@ end;
 
 function AUtils_FileExists(const FileName: AString_Type): ABoolean;
 begin
-  if not(Assigned(AUtilsProcVars.AUtils_FileExists)) then
+  if Assigned(AUtilsProcVars.AUtils_FileExists) then
   begin
-    Result := False;
+    Result := AUtilsProcVars.AUtils_FileExists(FileName);
     Exit;
   end;
-  Result := AUtilsProcVars.AUtils_FileExists(FileName);
+  {$ifdef ADepr}
+  if Assigned(AUtilsProcVars.AUtils_FileExistsWS) then
+  begin
+    Result := AUtilsProcVars.AUtils_FileExistsWS(AString_ToWideString(FileName));
+    Exit;
+  end;
+  {$endif}
+  Result := False;
+end;
+
+function AUtils_FileExistsP(const FileName: APascalString): ABoolean;
+var
+  S: AString_Type;
+begin
+  if Assigned(AUtilsProcVars.AUtils_FileExists) then
+  begin
+    if (AString_AssignP(S, FileName) < 0) then
+    begin
+      Result := False;
+      Exit;
+    end;
+    Result := AUtilsProcVars.AUtils_FileExists(S);
+    Exit;
+  end;
+  {$ifdef ADepr}
+  if Assigned(AUtilsProcVars.AUtils_FileExistsWS) then
+  begin
+    Result := AUtilsProcVars.AUtils_FileExistsWS(FileName);
+    Exit;
+  end;
+  {$endif}
+  Result := False;
 end;
 
 function AUtils_FileExistsWS(const FileName: AWideString): ABoolean;
 var
   S: AString_Type;
 begin
-  if (AString_AssignWS(S, FileName) < 0) then
+  if Assigned(AUtilsProcVars.AUtils_FileExists) then
   begin
-    Result := False;
+    if (AString_AssignWS(S, FileName) < 0) then
+    begin
+      Result := False;
+      Exit;
+    end;
+    Result := AUtilsProcVars.AUtils_FileExists(S);
     Exit;
   end;
-  Result := AUtils_FileExists(S);
+  {$ifdef ADepr}
+  if Assigned(AUtilsProcVars.AUtils_FileExistsWS) then
+  begin
+    Result := AUtilsProcVars.AUtils_FileExistsWS(FileName);
+    Exit;
+  end;
+  {$endif}
+  Result := False;
 end;
 
 function AUtils_GetNowDateTime(): TDateTime;
@@ -216,6 +315,43 @@ begin
   Result := AString_ToWideString(S);
 end;
 
+function AUtils_ReplaceCommaP(const S: APascalString; DecimalSeparator: AChar;
+    ClearSpace: ABoolean): APascalString;
+var
+  S1: AString_Type;
+  Res: AString_Type;
+begin
+  if Assigned(AUtilsProcVars.AUtils_ReplaceComma) then
+  begin
+    if (AString_AssignP(S1, S) < 0) then
+    begin
+      Result := S;
+      Exit;
+    end;
+    if (AString_Clear(Res) < 0) then
+    begin
+      Result := S;
+      Exit;
+    end;
+    if (AUtilsProcVars.AUtils_ReplaceComma(S1, DecimalSeparator, ClearSpace, Res) >= 0) then
+    begin
+      Result := AString_ToWideString(Res);
+      Exit;
+    end;
+  end;
+  {$IFDEF ADepr}
+  if Assigned(AUtilsProcVars.AUtils_ReplaceCommaWS) then
+  begin
+    try
+      Result := AUtilsProcVars.AUtils_ReplaceCommaWS(S, DecimalSeparator, ClearSpace);
+    except
+      Result := S;
+    end;
+  end;
+  {$ENDIF}
+  Result := S;
+end;
+
 function AUtils_ReplaceCommaWS(const S: AWideString; DecimalSeparator: AChar;
     ClearSpace: ABoolean): AWideString;
 var
@@ -273,6 +409,30 @@ begin
   Result := -1;
 end;
 
+function AUtils_StrToFloatDefP(const S: APascalString; DefValue: AFloat): AFloat;
+var
+  S1: AString_Type;
+begin
+  if Assigned(AUtilsProcVars.AUtils_StrToFloatDef) then
+  begin
+    if (AString_AssignP(S1, S) < 0) then
+    begin
+      Result := DefValue;
+      Exit;
+    end;
+    Result := AUtilsProcVars.AUtils_StrToFloatDef(S1, DefValue);
+    Exit;
+  end;
+  {$ifdef ADepr}
+  if Assigned(AUtilsProcVars.AUtils_StrToFloatDefWS) then
+  begin
+    Result := AUtilsProcVars.AUtils_StrToFloatDefWS(S, DefValue);
+    Exit;
+  end;
+  {$endif}
+  Result := DefValue;
+end;
+
 function AUtils_StrToFloatDefWS(const S: AWideString; DefValue: AFloat): AFloat;
 var
   S1: AString_Type;
@@ -287,11 +447,61 @@ begin
     Result := AUtilsProcVars.AUtils_StrToFloatDef(S1, DefValue);
     Exit;
   end;
+  {$ifdef ADepr}
   if Assigned(AUtilsProcVars.AUtils_StrToFloatDefWS) then
   begin
     Result := AUtilsProcVars.AUtils_StrToFloatDefWS(S, DefValue);
     Exit;
   end;
+  {$endif}
+  Result := DefValue;
+end;
+
+function AUtils_StrToFloatP(const Value: APascalString): AFloat;
+var
+  S: AString_Type;
+begin
+  if Assigned(AUtilsProcVars.AUtils_StrToFloat) then
+  begin
+    if (AString_AssignP(S, Value) < 0) then
+    begin
+      Result := 0;
+      Exit;
+    end;
+    Result := AUtilsProcVars.AUtils_StrToFloat(S);
+    Exit;
+  end;
+  {$ifdef ADepr}
+  if Assigned(AUtilsProcVars.AUtils_StrToFloatWS) then
+  begin
+    Result := AUtilsProcVars.AUtils_StrToFloatWS(Value);
+    Exit;
+  end;
+  {$endif}
+  Result := AUtils_StrToFloatDefP(Value, 0);
+end;
+
+function AUtils_StrToIntDefP(const S: APascalString; DefValue: AInt): AInt;
+var
+  S1: AString_Type;
+begin
+  if Assigned(AUtilsProcVars.AUtils_StrToIntDef) then
+  begin
+    if (AString_AssignP(S1, S) < 0) then
+    begin
+      Result := DefValue;
+      Exit;
+    end;
+    Result := AUtilsProcVars.AUtils_StrToIntDef(S1, DefValue);
+    Exit;
+  end;
+  {$IFDEF ADepr}
+  if Assigned(AUtilsProcVars.AUtils_StrToIntDefWS) then
+  begin
+    Result := AUtilsProcVars.AUtils_StrToIntDefWS(S, DefValue);
+    Exit;
+  end;
+  {$ENDIF}
   Result := DefValue;
 end;
 
@@ -319,6 +529,30 @@ begin
   Result := DefValue;
 end;
 
+function AUtils_StrToIntP(const Value: APascalString): AInt;
+var
+  S1: AString_Type;
+begin
+  if Assigned(AUtilsProcVars.AUtils_StrToInt) then
+  begin
+    if (AString_AssignP(S1, Value) < 0) then
+    begin
+      Result := 0;
+      Exit;
+    end;
+    Result := AUtilsProcVars.AUtils_StrToInt(S1);
+    Exit;
+  end;
+  {$ifdef ADepr}
+  if Assigned(AUtilsProcVars.AUtils_StrToIntWS) then
+  begin
+    Result := AUtilsProcVars.AUtils_StrToIntWS(Value);
+    Exit;
+  end;
+  {$endif}
+  Result := AUtils_StrToIntDefP(Value, 0);
+end;
+
 function AUtils_Trim(var S: AString_Type): AError;
 begin
   if not(Assigned(AUtilsProcVars.AUtils_Trim)) then
@@ -327,6 +561,23 @@ begin
     Exit;
   end;
   Result := AUtilsProcVars.AUtils_Trim(S);
+end;
+
+function AUtils_TrimP(const S: APascalString): APascalString;
+var
+  Str: AString_Type;
+begin
+  if (AString_AssignP(Str, S) < 0) then
+  begin
+    Result := S;
+    Exit;
+  end;
+  if (AUtils_Trim(Str) < 0) then
+  begin
+    Result := S;
+    Exit;
+  end;
+  Result := AString_ToPascalString(Str);
 end;
 
 function AUtils_TrimWS(const S: AWideString): AWideString;
