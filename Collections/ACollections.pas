30720fcfd84b51069de59663ffde6b78b8b070a7
Prof1983 <prof1983@yandex.ru>
2012-12-26 14:58:25 +0600

    Updated

--- Collections/ACollections.pas
+++ Collections/ACollections.pas
@@ -2,14 +2,16 @@
 @Abstract ACollections
 @Author Prof1983 <prof1983@ya.ru>
 @Created 01.08.2012
-@LastMod 08.08.2012
+@LastMod 26.12.2012
 }
 unit ACollections;
 
 interface
 
 uses
-  ABase, ACollectionsBase, ACollectionsProcVars;
+  ABase,
+  ABaseTypes,
+  ACollectionsProcVars;
 
 // --- StringList ---
 
---
+++ Settings/ASettingsMain.pas
@@ -0,0 +1,13 @@
+{**
+@Abstract ASettings
+@Author Prof1983 <prof1983@ya.ru>
+@Created 26.12.2012
+@LastMod 26.12.2012
+}
+unit ASettingsMain;
+
+interface
+
+implementation
+
+end.
