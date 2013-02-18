6156284969fc9552727704ebdbb22eaf043aca88
Prof1983 <prof1983@yandex.ru>
2013-02-18 10:01:13 +0600

    Updated AEvents

--- Common/AEventsProcTypes.pas
+++ Common/AEventsProcTypes.pas
@@ -2,7 +2,7 @@
 @Abstract AEvents - Procs
 @Author Prof1983 <prof1983@ya.ru>
 @Created 29.08.2011
-@LastMod 30.07.2012
+@LastMod 18.02.2013
 }
 unit AEventsProcTypes;
 
@@ -12,34 +12,20 @@ uses
   ABase;
 
 type
-  A_Event_New_Proc = function(Obj: Integer; const Name: AString_Type): AEvent; stdcall;
-  A_Event_NewA_Proc = function(Obj: Integer; {const} Name: PAnsiChar): AEvent; stdcall;
-  A_Event_NewP_Proc = function(Obj: Integer; const Name: APascalString): AEvent; stdcall;
-  A_Event_NewW_Proc = function(Obj: Integer; {const} Name: PWideChar): AEvent; stdcall;
-  A_Event_NewWS_Proc = function(Obj: Integer; const Name: AWideString): AEvent; stdcall;
-  A_Event_Free_Proc = function(Event: AEvent): AError; stdcall;
-  A_Event_Clear_Proc = function(Event: AEvent): AError; stdcall;
-  A_Event_GetListenersCount_Proc = function(Event: AEvent): AInteger; stdcall;
-
-  A_Event_GetName_Proc = function(Event: AEvent; out Value: AString_Type): AInteger; stdcall;
-  // MaxLen = SizeOf - 1
-  A_Event_GetNameA_Proc = function(Event: AEvent; {out} Value: PAnsiChar; MaxLen: AInteger): AInteger; stdcall;
-  A_Event_GetNameP_Proc = function(Event: AEvent): APascalString; stdcall;
-  // MaxLen = (SizeOf div 2) - 1
-  A_Event_GetNameW_Proc = function(Event: AEvent; {out} Value: PWideChar; MaxLen: AInteger): AInteger; stdcall;
-
-  A_Event_Connect_Proc = function(Event: AEvent; Callback: ACallbackProc; Weight: AInteger = High(AInteger)): AInteger; stdcall;
-  A_Event_Disconnect_Proc = function(Event: AEvent; Callback: ACallbackProc): AInteger; stdcall;
-  // Вызывает событие. Возврящает кол-во успешно выполненных вызовов (>0) или ошибку (<0).
-  A_Event_Invoke_Proc = function(Event: AEvent; Data: Integer): AInteger; stdcall;
-
-  {** Finalize }
-  A_Events_Fin_Proc = function(): AError; stdcall;
-
-  {** Initialize }
-  A_Events_Init_Proc = function(): AError; stdcall;
+  AEvent_Clear_Proc = function(Event: AEvent): AError; stdcall;
+  AEvent_Connect_Proc = function(Event: AEvent; Callback: ACallbackProc; Weight: AInt): AInt; stdcall;
+  AEvent_Disconnect_Proc = function(Event: AEvent; Callback: ACallbackProc): AInt; stdcall;
+  AEvent_Free_Proc = function(Event: AEvent): AError; stdcall;
+  AEvent_GetListenersCount_Proc = function(Event: AEvent): AInt; stdcall;
+  AEvent_GetName_Proc = function(Event: AEvent; out Value: AString_Type): AInt; stdcall;
+  AEvent_GetNameA_Proc = function(Event: AEvent; Value: AStr; MaxLen: AInt): AInt; stdcall;
+  AEvent_Invoke_Proc = function(Event: AEvent; Data: AInt): AInt; stdcall;
+  AEvent_New_Proc = function(Obj: Integer; const Name: AString_Type): AEvent; stdcall;
+  AEvent_NewA_Proc = function(Obj: Integer; Name: AStr): AEvent; stdcall;
+  AEvents_Fin_Proc = function(): AError; stdcall;
+  AEvents_Init_Proc = function(): AError; stdcall;
 
 implementation
-
+
 end.
  
--- Impl/AEventsMod.pas
+++ Impl/AEventsMod.pas
@@ -2,68 +2,32 @@
 @Abstract AEvents
 @Author Prof1983 <prof1983@ya.ru>
 @Created 27.05.2011
-@LastMod 15.11.2012
+@LastMod 18.02.2013
 }
 unit AEventsMod;
-
-{$IFDEF A04}{$DEFINE ADepr}{$ENDIF}
 
 interface
 
 uses
-  ABase, AEvents, AEventsBase, {$IFDEF ADepr}AEventsProcRec,{$ENDIF} ARuntimeMain, ARuntimeBase, AStrings;
+  ABase,
+  AEventsBase,
+  AEventsMain,
+  ARuntimeMain,
+  ARuntimeBase,
+  AStringMain;
 
 function AEventsMod_Boot(): AError; stdcall;
 
 function AEventsMod_Fin(): AError; stdcall;
 
+function AEventsMod_GetProc(ProcName: AStr): Pointer; stdcall;
+
 function AEventsMod_Init(): AError; stdcall;
-
-{$IFDEF ADepr}
-const
-  EventsProcs: AEventsProcs_Type = (
-    Event_Clear: AEvents.Event_Clear;                           // 00
-    Event_Connect: AEvents.Event_Connect;                       // 01
-    Event_Disconnect: AEvents.Event_Disconnect;                 // 02
-    Event_Free: AEvents.Event_Free;                             // 03
-    Event_GetListenersCount: AEvents.Event_GetListenersCount;   // 04
-    Event_GetName: AEvents.Event_GetName;                       // 05
-    Event_Invoke: AEvents.Event_Invoke;                         // 06
-    Event_New: AEvents.Event_New;                               // 07
-
-    Event_NewA: AEvents.Event_NewA;                             // 08
-    Event_NewP: AEvents.Event_NewP;                             // 09
-    Event_NewW: AEvents.Event_NewW;                             // 10
-    Event_GetNameA: AEvents.Event_GetNameA;                     // 11
-    Event_GetNameP: AEvents.Event_GetNameP;                     // 12
-    Event_GetNameW: AEvents.Event_GetNameW;                     // 13
-    Init: AEventsMod_Init;                                      // 14
-    Fin: AEventsMod_Fin;                                        // 15
-
-    Event_NewWS: AEvents.Event_NewWS;                           // 16
-    Reserved17: 0;
-    Reserved18: 0;
-    Reserved19: 0;
-    Reserved20: 0;
-    Reserved21: 0;
-    Reserved22: 0;
-    Reserved23: 0;
-
-    Reserved24: 0;
-    Reserved25: 0;
-    Reserved26: 0;
-    Reserved27: 0;
-    Reserved28: 0;
-    Reserved29: 0;
-    Reserved30: 0;
-    Reserved31: 0;
-    );
-{$ENDIF ADepr}
 
 implementation
 
 const
-  AEvents_Version = $00040000;
+  AEvents_Version = $00070000;
 
 const
   EventsModule: AModule_Type = (
@@ -73,8 +37,8 @@ const
     Description: nil;
     Init: AEventsMod_Init;
     Fin: AEventsMod_Fin;
-    GetProc: nil;
-    Procs: {$IFDEF ADepr}Addr(EventsProcs){$ELSE}nil{$ENDIF};
+    GetProc: AEventsMod_GetProc;
+    Procs: nil;
     );
 
 // --- AEventsMod ---
@@ -98,12 +62,42 @@ end;
 
 function AEventsMod_Fin(): AError;
 begin
-  Result := 0;
+  Result := AEvents_Fin();
+end;
+
+function AEventsMod_GetProc(ProcName: AStr): Pointer;
+begin
+  if (ProcName = 'AEvent_Clear') then
+    Result := Addr(AEvent_Clear)
+  else if (ProcName = 'AEvent_Connect') then
+    Result := Addr(AEvent_Connect)
+  else if (ProcName = 'AEvent_Disconnect') then
+    Result := Addr(AEvent_Disconnect)
+  else if (ProcName = 'AEvent_Free') then
+    Result := Addr(AEvent_Free)
+  else if (ProcName = 'AEvent_GetListenersCount') then
+    Result := Addr(AEvent_GetListenersCount)
+  else if (ProcName = 'AEvent_GetName') then
+    Result := Addr(AEvent_GetName)
+  else if (ProcName = 'AEvent_GetNameA') then
+    Result := Addr(AEvent_GetNameA)
+  else if (ProcName = 'AEvent_Invoke') then
+    Result := Addr(AEvent_Invoke)
+  else if (ProcName = 'AEvent_New') then
+    Result := Addr(AEvent_New)
+  else if (ProcName = 'AEvent_NewA') then
+    Result := Addr(AEvent_NewA)
+  else if (ProcName = 'AEvents_Fin') then
+    Result := Addr(AEvents_Fin)
+  else if (ProcName = 'AEvents_Init') then
+    Result := Addr(AEvents_Init)
+  else
+    Result := nil;
 end;
 
 function AEventsMod_Init(): AError;
 begin
-  Result := 0;
+  Result := AEvents_Init();
 end;
 
 end.
