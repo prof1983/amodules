bd2d0a05393a59ab896d9dbbb0b758d71eceda62
Prof1983 <prof1983@yandex.ru>
2013-02-20 15:06:20 +0600

    Updated AUiSplashMod

--- Impl/AUiSplashMod.pas
+++ Impl/AUiSplashMod.pas
@@ -2,70 +2,34 @@
 @Abstract AUiSplash
 @Author Prof1983 <prof1983@ya.ru>
 @Created 08.12.2009
-@LastMod 14.12.2012
+@LastMod 18.02.2013
 }
 unit AUiSplashMod;
-
-{$IFDEF A04}{$DEFINE ADepr}{$ENDIF}
 
 interface
 
 uses
-  ABase, ARuntimeBase, ARuntimeMain, ASettings, ASettingsBase, ASystem,
-  AUi, AUiBase, AUiSplash, AUiSplashBase, AUtils, AUtilsBase;
+  ABase,
+  ARuntimeBase,
+  ARuntimeMain,
+  ASettingsBase,
+  AUiBase,
+  AUiSplashBase,
+  AUiSplashMain,
+  AUtilsBase;
 
 function AUiSplashMod_Boot(): AError; stdcall;
 
 function AUiSplashMod_Fin(): AError; stdcall;
 
+function AUiSplashMod_GetProc(ProcName: AStr): Pointer; stdcall;
+
 function AUiSplashMod_Init(): AError; stdcall;
 
 implementation
 
-{$IFDEF ADepr}
-type
-  AUiSplashProcsRec = packed record {8x4}
-    Reserved00: AInteger;
-    Reserved01: AInteger;
-    Reserved02: AInteger;
-    Reserved03: AInteger;
-    Reserved04: AInteger;
-    Reserved05: AInteger;
-    Reserved06: AInteger;
-    Reserved07: AInteger;
-    Reserved08: AInteger;
-    Reserved09: AInteger;
-    Reserved10: AInteger;
-    Reserved11: AInteger;
-    Reserved12: AInteger;
-    Reserved13: AInteger;
-    Reserved14: AInteger;
-    Reserved15: AInteger;
-  end;
-
 const
-  UiSplashProcs: AUiSplashProcsRec = (
-    Reserved00: 0;
-    Reserved01: 0;
-    Reserved02: 0;
-    Reserved03: 0;
-    Reserved04: 0;
-    Reserved05: 0;
-    Reserved06: 0;
-    Reserved07: 0;
-    Reserved08: 0;
-    Reserved09: 0;
-    Reserved10: 0;
-    Reserved11: 0;
-    Reserved12: 0;
-    Reserved13: 0;
-    Reserved14: 0;
-    Reserved15: 0;
-    );
-{$ENDIF ADepr}
-
-const
-  AUiSplash_Version = $00050000;
+  AUiSplash_Version = $00070000;
 
 const
   UiSplashModule: AModule_Type = (
@@ -75,8 +39,8 @@ const
     Description: nil;
     Init: AUiSplashMod_Init;
     Fin: AUiSplashMod_Fin;
-    GetProc: nil;
-    Procs: {$IFDEF ADepr}Addr(UiSplashProcs){$ELSE}nil{$ENDIF};
+    GetProc: AUiSplashMod_GetProc;
+    Procs: nil;
     );
 
 // --- AUiSplashMod ---
@@ -90,6 +54,22 @@ function AUiSplashMod_Fin(): AError;
 begin
   Result := AUiSplash_Fin();
   ARuntime_DeleteModuleByUid(AUiSplash_Uid);
+end;
+
+function AUiSplashMod_GetProc(ProcName: AStr): Pointer;
+begin
+  if (ProcName = 'AUiSplash_Fin') then
+    Result := Addr(AUiSplash_Fin)
+  else if (ProcName = 'AUiSplash_Hide') then
+    Result := Addr(AUiSplash_Hide)
+  else if (ProcName = 'AUiSplash_Init') then
+    Result := Addr(AUiSplash_Init)
+  else if (ProcName = 'AUiSplash_Sleep') then
+    Result := Addr(AUiSplash_Sleep)
+  else if (ProcName = 'AUiSplash_StepIt') then
+    Result := Addr(AUiSplash_StepIt)
+  else
+    Result := nil;
 end;
 
 function AUiSplashMod_Init(): AError;
