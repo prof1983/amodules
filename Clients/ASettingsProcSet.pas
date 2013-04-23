00bc74929819a71e82be3b6b98c2a567fa39899e
Prof1983 <prof1983@yandex.ru>
2013-04-23 15:05:54 +0600

    Updated ASettings

--- Clients/ASettingsProcSet.pas
+++ Clients/ASettingsProcSet.pas
@@ -2,20 +2,70 @@
 @Abstract ASettings
 @Author Prof1983 <prof1983@ya.ru>
 @Created 19.11.2012
-@LastMod 19.11.2012
+@LastMod 23.04.2013
 }
 unit ASettingsProcSet;
 
 interface
 
 uses
-  ABase, ASettingsProcRec, ASettingsProcVars;
+  ABase,
+  ARuntimeBase,
+  {$ifdef ADepr}
+  ASettingsProcRec,
+  {$endif}
+  ASettingsProcVars;
 
-function ASettings_SetProcs(const Settings: ASettingsProcs_Type): AError;
+function ASettings_SetProcs(GetProc: AModuleGetProc): AError;
+
+{$ifdef ADepr}
+function ASettings_SetProcsR(const Settings: ASettingsProcs_Type): AError;
+{$endif}
 
 implementation
 
-function ASettings_SetProcs(const Settings: ASettingsProcs_Type): AError;
+function ASettings_SetProcs(GetProc: AModuleGetProc): AError;
+begin
+  ASettingsProcVars.ASettings_Close := GetProc('ASettings_Close');
+  ASettingsProcVars.ASettings_DeleteKey := GetProc('ASettings_DeleteKey');
+  ASettingsProcVars.ASettings_DeleteKeyA := GetProc('ASettings_DeleteKeyA');
+  ASettingsProcVars.ASettings_DeleteSection := GetProc('ASettings_DeleteSection');
+  ASettingsProcVars.ASettings_DeleteSectionA := GetProc('ASettings_DeleteSectionA');
+  ASettingsProcVars.ASettings_Fin := GetProc('ASettings_Fin');
+  ASettingsProcVars.ASettings_Init := GetProc('ASettings_Init');
+  ASettingsProcVars.ASettings_NewIniConfig := GetProc('ASettings_NewIniConfig');
+  ASettingsProcVars.ASettings_NewIniConfigA := GetProc('ASettings_NewIniConfigA');
+  ASettingsProcVars.ASettings_NewRegConfig := GetProc('ASettings_NewRegConfig');
+  ASettingsProcVars.ASettings_NewRegConfigA := GetProc('ASettings_NewRegConfigA');
+  ASettingsProcVars.ASettings_ReadBoolDef := GetProc('ASettings_ReadBoolDef');
+  ASettingsProcVars.ASettings_ReadBoolDefA := GetProc('ASettings_ReadBoolDefA');
+  ASettingsProcVars.ASettings_ReadDateTimeDef := GetProc('ASettings_ReadDateTimeDef');
+  ASettingsProcVars.ASettings_ReadDateTimeDefA := GetProc('ASettings_ReadDateTimeDefA');
+  ASettingsProcVars.ASettings_ReadFloatDef := GetProc('ASettings_ReadFloatDef');
+  ASettingsProcVars.ASettings_ReadFloatDefA := GetProc('ASettings_ReadFloatDefA');
+  ASettingsProcVars.ASettings_ReadIntDef := GetProc('ASettings_ReadIntDef');
+  ASettingsProcVars.ASettings_ReadIntDefA := GetProc('ASettings_ReadIntDefA');
+  ASettingsProcVars.ASettings_ReadSection := GetProc('ASettings_ReadSection');
+  ASettingsProcVars.ASettings_ReadSectionA := GetProc('ASettings_ReadSectionA');
+  ASettingsProcVars.ASettings_ReadString := GetProc('ASettings_ReadString');
+  ASettingsProcVars.ASettings_ReadStringA := GetProc('ASettings_ReadStringA');
+  ASettingsProcVars.ASettings_ReadStringDef := GetProc('ASettings_ReadStringDef');
+  ASettingsProcVars.ASettings_ReadStringDefA := GetProc('ASettings_ReadStringDefA');
+  ASettingsProcVars.ASettings_WriteBool := GetProc('ASettings_WriteBool');
+  ASettingsProcVars.ASettings_WriteBoolA := GetProc('ASettings_WriteBoolA');
+  ASettingsProcVars.ASettings_WriteDateTime := GetProc('ASettings_WriteDateTime');
+  ASettingsProcVars.ASettings_WriteDateTimeA := GetProc('ASettings_WriteDateTimeA');
+  ASettingsProcVars.ASettings_WriteFloat := GetProc('ASettings_WriteFloat');
+  ASettingsProcVars.ASettings_WriteFloatA := GetProc('ASettings_WriteFloatA');
+  ASettingsProcVars.ASettings_WriteInt := GetProc('ASettings_WriteInt');
+  ASettingsProcVars.ASettings_WriteIntA := GetProc('ASettings_WriteIntA');
+  ASettingsProcVars.ASettings_WriteString := GetProc('ASettings_WriteString');
+  ASettingsProcVars.ASettings_WriteStringA := GetProc('ASettings_WriteStringA');
+  Result := 0;
+end;
+
+{$ifdef ADepr}
+function ASettings_SetProcsR(const Settings: ASettingsProcs_Type): AError;
 begin
   ASettingsProcVars.ASettings_Close02 := Settings.Config_Close;
   ASettingsProcVars.ASettings_DeleteKey02 := Settings.DeleteKeyW;
@@ -34,6 +84,7 @@ begin
   ASettingsProcVars.ASettings_WriteDateTimeWS := Settings.WriteDateTimeW;
   Result := 0;
 end;
+{$endif}
 
 end.
  
--- Common/ASettingsProcTypes.pas
+++ Common/ASettingsProcTypes.pas
@@ -2,7 +2,7 @@
 @Abstract ASettings proc types
 @Author Prof1983 <prof1983@ya.ru>
 @Created 23.07.2010
-@LastMod 24.12.2012
+@LastMod 23.04.2013
 }
 unit ASettingsProcTypes;
 
@@ -20,23 +20,44 @@ uses
 type
   ASettings_Close_Proc = function(Config: AConfig): AError; stdcall;
   ASettings_DeleteKey_Proc = function(Config: AConfig; const Section, Name: AString_Type): AError; stdcall;
+  ASettings_DeleteKeyA_Proc = function(Config: AConfig; Section, Name: AStr): AError; stdcall;
   ASettings_DeleteSection_Proc = function(Config: AConfig; const Section: AString_Type): AError; stdcall;
+  ASettings_DeleteSectionA_Proc = function(Config: AConfig; Section: AStr): AError; stdcall;
   ASettings_Fin_Proc = function(): AError; stdcall;
-  ASettings_IniConfig_New_Proc = function(const FileName: AString_Type): AConfig; stdcall;
   ASettings_Init_Proc = function(): AError; stdcall;
-  ASettings_ReadBoolDef_Proc = function(Config: AConfig; const Section, Name: AString_Type; DefValue: ABoolean): ABoolean; stdcall;
+  ASettings_NewIniConfig_Proc = function(const FileName: AString_Type): AConfig; stdcall;
+  ASettings_NewIniConfigA_Proc = function(FileName: AStr): AConfig; stdcall;
+  ASettings_NewRegConfig_Proc = function(const Prefix: AString_Type; Key: AInt): AConfig; stdcall;
+  ASettings_NewRegConfigA_Proc = function(Prefix: AStr; Key: AInt): AConfig; stdcall;
+  ASettings_ReadBoolDef_Proc = function(Config: AConfig; const Section, Name: AString_Type; DefValue: ABool): ABool; stdcall;
+  ASettings_ReadBoolDefA_Proc = function(Config: AConfig; Section, Name: AStr; DefValue: ABool): ABool; stdcall;
   ASettings_ReadDateTimeDef_Proc = function(Config: AConfig; const Section, Name: AString_Type; DefValue: TDateTime): TDateTime; stdcall;
-  ASettings_ReadIntegerDef_Proc = function(Config: AConfig; const Section, Name: AString_Type; DefValue: AInteger): AInteger; stdcall;
+  ASettings_ReadDateTimeDefA_Proc = function(Config: AConfig; Section, Name: AStr; DefValue: TDateTime): TDateTime; stdcall;
   ASettings_ReadFloatDef_Proc = function(Config: AConfig; const Section, Name: AString_Type; DefValue: AFloat): AFloat; stdcall;
+  ASettings_ReadFloatDefA_Proc = function(Config: AConfig; Section, Name: AStr; DefValue: AFloat): AFloat; stdcall;
+  ASettings_ReadIntDef_Proc = function(Config: AConfig; const Section, Name: AString_Type; DefValue: AInt): AInt; stdcall;
+  ASettings_ReadIntDefA_Proc = function(Config: AConfig; Section, Name: AStr; DefValue: AInt): AInt; stdcall;
   ASettings_ReadSection_Proc = function(Config: AConfig; const Section: AString_Type; Strings: AStringList): AError; stdcall;
-  ASettings_ReadStringDef_Proc = function(Config: AConfig; const Section, Name, DefValue: AString_Type; out Value: AString_Type): AInteger; stdcall;
-  ASettings_RegConfig_New_Proc = function(const Prefix: AString_Type): AConfig; stdcall;
-  ASettings_RegConfig_New2_Proc = function(const Prefix: AString_Type; HKEY: AInteger): AConfig; stdcall;
-  ASettings_WriteBool_Proc = function(Config: AConfig; const Section, Name: AString_Type; Value: ABoolean): AError; stdcall;
+  ASettings_ReadSectionA_Proc = function(Config: AConfig; Section: AStr; Strings: AStringList): AError; stdcall;
+  ASettings_ReadString_Proc = function(Config: AConfig; const Section, Name: AString_Type; out Value: AString_Type): AInt; stdcall;
+  ASettings_ReadStringA_Proc = function(Config: AConfig; Section, Name: AStr; Value: AStr; MaxLen: AInt): AInt; stdcall;
+  ASettings_ReadStringDef_Proc = function(Config: AConfig; const Section, Name, DefValue: AString_Type; out Value: AString_Type): AInt; stdcall;
+  ASettings_ReadStringDefA_Proc = function(Config: AConfig; Section, Name, DefValue: AStr; Value: AStr; MaxLen: AInt): AInt; stdcall;
+  ASettings_WriteBool_Proc = function(Config: AConfig; const Section, Name: AString_Type; Value: ABool): AError; stdcall;
+  ASettings_WriteBoolA_Proc = function(Config: AConfig; Section, Name: AStr; Value: ABool): AError; stdcall;
   ASettings_WriteDateTime_Proc = function(Config: AConfig; const Section, Name: AString_Type; Value: TDateTime): AError; stdcall;
+  ASettings_WriteDateTimeA_Proc = function(Config: AConfig; Section, Name: AStr; Value: TDateTime): AError; stdcall;
   ASettings_WriteFloat_Proc = function(Config: AConfig; const Section, Name: AString_Type; Value: AFloat): AError; stdcall;
-  ASettings_WriteInteger_Proc = function(Config: AConfig; const Section, Name: AString_Type; Value: AInteger): AError; stdcall;
+  ASettings_WriteFloatA_Proc = function(Config: AConfig; Section, Name: AStr; Value: AFloat): AError; stdcall;
+  ASettings_WriteInt_Proc = function(Config: AConfig; const Section, Name: AString_Type; Value: AInt): AError; stdcall;
+  ASettings_WriteIntA_Proc = function(Config: AConfig; Section, Name: AStr; Value: AInt): AError; stdcall;
   ASettings_WriteString_Proc = function(Config: AConfig; const Section, Name, Value: AString_Type): AError; stdcall;
+  ASettings_WriteStringA_Proc = function(Config: AConfig; Section, Name, Value: AStr): AError; stdcall;
+  //ASettings_IniConfig_New_Proc = ASettings_NewIniConfig_Proc;
+  //ASettings_ReadIntegerDef_Proc = ASettings_ReadIntDef_Proc;
+  //ASettings_RegConfig_New_Proc = function(const Prefix: AString_Type): AConfig; stdcall;
+  //ASettings_RegConfig_New2_Proc = ASettings_NewRegConfig_Proc;
+  //ASettings_WriteInteger_Proc = ASettings_WriteInt_Proc;
   {$IFDEF ADepr}
   ASettings_Close02_Proc = procedure(Config: AConfig); stdcall;
   ASettings_DeleteKey02_Proc = function(Config: AConfig; const Section, Name: AWideString): ABoolean; stdcall;
