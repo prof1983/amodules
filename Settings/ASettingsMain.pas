31c13ae19662abdc37c3bec6eb21602235323373
Prof1983 <prof1983@yandex.ru>
2013-04-24 17:14:24 +0600

    Added ASettings_Fin(), ASettings_Init()

--- Settings/ASettingsMain.pas
+++ Settings/ASettingsMain.pas
@@ -2,12 +2,28 @@
 @Abstract ASettings
 @Author Prof1983 <prof1983@ya.ru>
 @Created 26.12.2012
-@LastMod 26.12.2012
 }
 unit ASettingsMain;
 
 interface
 
+uses
+  ABase;
+
+function ASettings_Fin(): AError; stdcall;
+
+function ASettings_Init(): AError; stdcall;
+
 implementation
+
+function ASettings_Fin(): AError;
+begin
+  Result := 0;
+end;
+
+function ASettings_Init(): AError;
+begin
+  Result := 0;
+end;
 
 end.
